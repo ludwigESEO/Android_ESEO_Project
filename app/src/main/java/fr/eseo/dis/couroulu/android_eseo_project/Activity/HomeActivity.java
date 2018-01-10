a6fa865cb835411eb73409e40f293409f8186a5f
Florent <florent.dupe@reseau.eseo.fr>
2018-01-10 08:50:57 +0100

    commit chain.crt and home

--- app/src/main/java/fr/eseo/dis/couroulu/android_eseo_project/Activity/HomeActivity.java
+++ app/src/main/java/fr/eseo/dis/couroulu/android_eseo_project/Activity/HomeActivity.java
@@ -1,4 +1,4 @@
-package fr.eseo.dis.couroulu.android_eseo_project;
+package fr.eseo.dis.couroulu.android_eseo_project.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import fr.eseo.dis.couroulu.android_eseo_project.R;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
